57e6f256f90f0be953db6b60dc3d515da1a258cb
sidnet1964 <sidnet@mail.ru>
2021-08-03 00:39:43 +0300

    lesson and added

--- js/app.js
+++ js/app.js
@@ -1,84 +1,55 @@
 'use strict';
-//  модуль расчета
-function calculatePercent(periodValue){
-    if (periodValue < 3) {return 0;}
-    if (periodValue < 6) {return 2;}
-    if (periodValue < 9) {return 2.2;}
-    if (periodValue < 12) {return 2.3;}
-    if (periodValue < 18) {return 2.6;}
-    if (periodValue === 18) {return 2.7;}
-//  больше 18 месяцев нельзя, или будет 2.7
-    return 0;
-}
-function calculateDeposit(amountInputValue, periodInputValue){
-    const percentValue = calculatePercent(periodInputValue);
-    const monthInYear = 12;
-    const totalValue = amountInputValue * ((1 + percentValue / (monthInYear * 100 ))**periodInputValue);
-    const profitValue = totalValue - amountInputValue;
-//  {x * (1+s/(12*100))^{m}}
-//  где x — начальная сумма вклада, s — годовая ставка в процентах, m — срок вклада в месяцах.
-    return {
-        totalValue,
-        profitValue,
-        percentValue,
-    };
-}
+
 function handleSubmit(evt){
     evt.preventDefault();
 
-    amountErrorEl.textContent = '';
-    periodErrorEl.textContent = '';
-    totalDepositEl.textContent = '';
-    profitDepositEl.textContent = '';
-    percentDepositEl.textContent = '';
+    // phoneErrorEl.textContent = '';
+    // emailErrorEl.textContent = '';
+    // totalDepositEl.textContent = '';
+    // profitDepositEl.textContent = '';
+    // percentDepositEl.textContent = '';
 
-    const amountInput = Number(amountInputEl.value);
-    if (Number.isNaN(amountInput)){
-        amountErrorEl.textContent = 'Неверное значение. Введите число, например: 15000';
-        return;
-    }
-    if (amountInput < 15000){
-        amountErrorEl.textContent = 'Неверное значение. Минимальная сумма: 15000 ₽';
-        return;
-    }
-    if (amountInput > 50000000){
-        amountErrorEl.textContent = 'Неверное значение. Максимальная сумма: 50000000 ₽';
-        return;
-    }
+    const phone = phoneInputEl.value.trim();
+    const email = emailInputEl.value.trim();
 
-    const periodInput = Number(periodInputEl.value);
-    if (Number.isNaN(periodInput)){
-        periodErrorEl.textContent = 'Неверное значение. Введите число месяцев, например: 3';
-        return;
-    }
-    if (periodInput < 3){
-        periodErrorEl.textContent = 'Неверное значение. Минимальный период: 3 месяца';
-        return;
-    }
-    if (periodInput > 18){
-        periodErrorEl.textContent = 'Неверное значение. Максимальный период: 18 месяцев';
+    const data = {
+        phone,
+        email,
+    };
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://localhost:9999/api/lection/cards');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = handleSuccess;
+    xhr.onerror = handleError;
+    xhr.send((JSON.stringify(data)));
+}
+function handleSuccess(evt){
+    if (evt.target.status !== 200) {
+        // TODO: handle error
         return;
     }
 
-    const result = calculateDeposit(amountInput, periodInput);
-    totalDepositEl.textContent = result.totalValue.toFixed(0);
-    profitDepositEl.textContent = result.profitValue.toFixed(0);
-    percentDepositEl.textContent = result.percentValue.toFixed(1);
+    const data = JSON.parse(evt.target.responseText);
+    // TODO: work with data
 }
 
-const formEl = document.getElementById('deposit-form');
-// formEl.onsubmit = handleSubmit;
+function handleError(evt){
+    // TODO: handle error
+}
+
+const formEl = document.getElementById('card-form');
 formEl.addEventListener('submit', handleSubmit);
 
 //  входные значения
-const amountInputEl = document.getElementById('amount-input');
-const periodInputEl = document.getElementById('period-input');
+const phoneInputEl = document.getElementById('phone-input');
+const emailInputEl = document.getElementById('email-input');
 
 //  обработка ошибок
-const amountErrorEl = document.getElementById('amount-error');
-const periodErrorEl = document.getElementById('period-error');
+const phoneErrorEl = document.getElementById('phone-error');
+const emailErrorEl = document.getElementById('email-error');
 
 //  выходные значения
-const totalDepositEl = document.getElementById('total');
-const profitDepositEl = document.getElementById('profit');
-const percentDepositEl = document.getElementById('percent');
+// const totalDepositEl = document.getElementById('total');
+// const profitDepositEl = document.getElementById('profit');
+// const percentDepositEl = document.getElementById('percent');
